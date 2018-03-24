6c8d6e90a7a79ee9afeb2faf547747b3c734a097
Cosmin Popovici <hellocosmin@gmail.com>
2018-03-24 15:16:22 +0200

    Extract navigation generator to class.

--- bootstrap.php
+++ bootstrap.php
@@ -1,61 +1,15 @@
 <?php
 
-$dotenv = new \Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+use Dotenv\Dotenv;
+use App\Listeners\GenerateNavigation;
 
 /** @var $container \Illuminate\Container\Container */
 /** @var $events \TightenCo\Jigsaw\Events\EventBus */
 /** @var $jigsaw \TightenCo\Jigsaw\Jigsaw */
 
-$events->afterCollections(function ($jigsaw) {
+$dotenv = new Dotenv(__DIR__);
+$dotenv->load();
 
-    $env = getenv('NODE_ENV');
-    $collections = $jigsaw->getCollections();
-
-    $collections->each(function ($item, $key) use ($jigsaw, $env) {
-
-        $menu = [
-            'categories' => [],
-            'uncategorized' => []
-        ];
-
-        foreach ($item as $page => $data) {
-
-            if ($data->has('navigation')) {
-
-                $path = $env == 'offline' ? '../' . $data->_meta->collection . '/' . $data->_meta->filename . '.html' : $data->_meta->path->first();
-
-                if (isset($data->navigation['group'])) {
-                    $menu['categories'][$data->navigation['group']][] = [
-                        'title' => $data->title,
-                        'path' => $path,
-                        'order' => isset($data->navigation['order']) && is_numeric($data->navigation['order']) ? $data->navigation['order'] : 2e10,
-                    ];
-                    usort($menu['categories'][$data->navigation['group']], create_function('$a, $b', '
-                            $a = $a["order"];
-                            $b = $b["order"];
-                            if ($a == $b) return 0;
-                            return ($a < $b) ? -1 : 1;
-                        '));
-                } else {
-                    $menu['uncategorized'][] = [
-                        'title' => $data->title,
-                        'path' => $path,
-                        'order' => isset($data->navigation['order']) && is_numeric($data->navigation['order']) ? $data->navigation['order'] : 2e10,
-                    ];
-                    usort($menu['uncategorized'], create_function('$a, $b', '
-                            $a = $a["order"];
-                            $b = $b["order"];
-                            if ($a == $b) return 0;
-                            return ($a < $b) ? -1 : 1;
-                        '));
-                }
-
-            }
-        }
-
-        $jigsaw->setConfig('collections.'.$key.'.nav', $menu);
-    });
-});
+$events->afterCollections(GenerateNavigation::class);
 
 
