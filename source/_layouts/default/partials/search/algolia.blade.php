4aa1be606cf444cb4ebe79b3ce8a1cc9182c168b
Cosmin Popovici <hellocosmin@gmail.com>
2018-03-26 18:35:02 +0300

    More cosmetics for Algolia search.

--- source/_layouts/default/partials/search/algolia.blade.php
+++ source/_layouts/default/partials/search/algolia.blade.php
@@ -1,34 +1,33 @@
 <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
 <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
 <script>
-var client = algoliasearch('{{ $page->search->algolia->appID }}', '{{ $page->search->algolia->searchKey }}');
-var index = client.initIndex('{{ $page->search->algolia->indexName }}');
-autocomplete('#search',
-    {
-        debug: true,
-        hint: false
-    },
-    {
-    source: autocomplete.sources.hits(index, {hitsPerPage: 5}),
-    templates: {
-        header: function () {
-            return `<div class="flex justify-between items-center py-2 px-4">
-                        <span class="block text-sm text-grey-dark">Search results:</span>
-                        <a href="https://www.algolia.com" target="_blank" rel="noopener" class="aa-footer-logo">Search by Algolia</a>
-                    </div>`;
+    var client = algoliasearch('{{ $page->search->algolia->appID }}', '{{ $page->search->algolia->searchKey }}');
+    var index = client.initIndex('{{ $page->search->algolia->indexName }}');
+    autocomplete('#search',
+        {
+            hint: false
         },
-        suggestion: function(suggestion) {
-            var path = 'offline' == '{{ $env }}' ? suggestion.path.split('/').pop() + '.html' : '/' + suggestion.path ;
+        {
+        source: autocomplete.sources.hits(index, {hitsPerPage: 5}),
+        templates: {
+            header: function () {
+                return `<div class="flex justify-between items-center py-2 px-4">
+                            <span class="block text-sm text-grey-dark">Search results:</span>
+                            <a href="https://www.algolia.com" target="_blank" rel="noopener" class="aa-footer-logo">Search by Algolia</a>
+                        </div>`;
+            },
+            suggestion: function(suggestion) {
+                var path = 'offline' == '{{ $env }}' ? suggestion.path.split('/').pop() + '.html' : '/' + suggestion.path ;
 
-            return `<a href="${path}" class="block p-4">
-                        <span class="block text-sm">${suggestion._highlightResult.title.value}</span>
-                        <span class="block text-xs text-grey">${suggestion._highlightResult.description.value}</span>
-                    </a>`;
+                return `<a href="${path}" class="block p-4 hover:text-{{ $page->theme }}">
+                            <span class="block text-sm">${suggestion._highlightResult.title.value}</span>
+                            <span class="block text-xs text-grey">${suggestion._highlightResult.description.value}</span>
+                        </a>`;
+            }
         }
-    }
-}).on('autocomplete:selected', function (e, suggestion, dataset) {
-    var path = 'offline' == '{{ $env }}' ? suggestion.path.split('/').pop() + '.html' : '/' + suggestion.path ;
-    location.assign(path);
-});
+    }).on('autocomplete:selected', function (e, suggestion, dataset) {
+        var path = 'offline' == '{{ $env }}' ? suggestion.path.split('/').pop() + '.html' : '/' + suggestion.path ;
+        location.assign(path);
+    });
 </script>
 
