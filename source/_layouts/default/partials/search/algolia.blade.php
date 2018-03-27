c223722aa1452b26bb4057430d16e25d1d9d9a74
Cosmin Popovici <hellocosmin@gmail.com>
2018-03-27 11:03:13 +0300

    Downgrade Algolia JS to make it work with idiotic browsers. Like IE.

--- source/_layouts/default/partials/search/algolia.blade.php
+++ source/_layouts/default/partials/search/algolia.blade.php
@@ -11,18 +11,18 @@
         source: autocomplete.sources.hits(index, {hitsPerPage: 5}),
         templates: {
             header: function () {
-                return `<div class="flex justify-between items-center py-2 px-4">
-                            <span class="block text-sm text-grey-dark">Search results:</span>
-                            <a href="https://www.algolia.com" target="_blank" rel="noopener" class="aa-footer-logo">Search by Algolia</a>
-                        </div>`;
+                return '<div class="flex justify-between items-center py-2 px-4">'
+                        + '<span class="block text-sm text-grey-dark">Search results:</span>'
+                        + '<a href="https://www.algolia.com" target="_blank" rel="noopener" class="aa-footer-logo">Search by Algolia</a>'
+                        + '</div>';
             },
             suggestion: function(suggestion) {
                 var path = 'offline' == '{{ $env }}' ? suggestion.path.split('/').pop() + '.html' : '/' + suggestion.path ;
 
-                return `<a href="${path}" class="block p-4 hover:text-{{ $page->theme }}">
-                            <span class="block text-sm">${suggestion._highlightResult.title.value}</span>
-                            <span class="block text-xs text-grey">${suggestion._highlightResult.description.value}</span>
-                        </a>`;
+                return '<a href="'+path+'" class="block p-4 hover:text-{{ $page->theme }}">'
+                        + '<span class="block text-sm">'+suggestion._highlightResult.title.value+'</span>'
+                        + '<span class="block text-xs text-grey">'+suggestion._highlightResult.description.value+'</span>'
+                        + '</a>';
             }
         }
     }).on('autocomplete:selected', function (e, suggestion, dataset) {
