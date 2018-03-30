d5d9b2e2a6712c425d4e83524855716c1a23ef9e
Cosmin Popovici <hellocosmin@gmail.com>
2018-03-30 21:33:06 +0300

    Update changelog page.

--- source/_sartre/changelog.blade.php
+++ source/_sartre/changelog.blade.php
@@ -7,28 +7,40 @@ search: false
 page_order: 2000
 ---
 
+@php
+$pageCollection = $page->collections[$page->getCollection()];
+$changelog = collect($pageCollection->changelog);
+$changelog = $changelog->reverse();
+@endphp
+
 @section('body')
 
     @include('_layouts.default.partials.headers.page')
 
     <div class="container flex flex-row">
-        <main class="content w-full pt-32">
-
-            <h1 class="border-b mb-6">{{ $page->title }}</h1>
-
-        @foreach($page->collections[$page->getCollection()]->changelog as $key => $entry)
-
-            <h2 class="mb-0 pb-0">v{{ $entry['version'] }}</h2>
-            <span class="text-grey-dark text-xs">Released {{ date('F j, Y', $entry['date']) }}</span>
-            <div class="mt-6">{!! $entry['content'] !!}</div>
-
-        @endforeach
+        <main class="content w-full pt-32 pr-8">
+            <div class="changelog pl-16 -mt-12">
+                @foreach($changelog as $key => $entry)
+                    <div class="flex">
+                        <div class="w-1/5 border-r text-right py-12 pr-12">
+                            <h2 class="p-0">
+                                v{{ $entry['version'] }}
+                                <span class="dot absolute rounded-full bg-{{ $page->color }}"></span>
+                            </h2>
+                            <span class="text-grey-dark text-xs">{{ date('F j, Y', $entry['date']) }}</span>
+                        </div>
+                        <div class="py-12 px-12 w-full  {{ $loop->last ? '' : 'border-b' }}">
+                            <div class="">{!! $entry['content'] !!}</div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </main>
 
-        <aside class="w-1/3 sidebar-navigation mt-20">
-            <div class="pl-16 sticky top-20">
+        <aside class="w-1/5 sidebar-navigation mt-20">
+            <div class="pl-8 sticky top-20">
                 <div class="overflow-y-auto wrapper py-12">
-                    <table-of-contents title="Versions"></table-of-contents>
+                    <table-of-contents title="Changelog"></table-of-contents>
                 </div>
             </div>
         </aside>
@@ -38,5 +50,5 @@ page_order: 2000
 
 @push('scripts')
     <script src="@mix('/js/app.js')"></script>
-    @include('_layouts.default.partials.search.algolia', ['page' => $page->collections[$page->getCollection()]])
+    @include('_layouts.default.partials.search.algolia', ['page' => $pageCollection])
 @endpush
