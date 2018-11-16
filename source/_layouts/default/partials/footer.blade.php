394e532c25d57a0712d6089497de0110607611d0
Cosmin Popovici <hellocosmin@gmail.com>
2018-11-16 13:38:38 +0200

    Simplify footer, for now.

--- source/_layouts/default/partials/footer.blade.php
+++ source/_layouts/default/partials/footer.blade.php
@@ -1,64 +1,11 @@
 <footer class="bg-white py-8 lg:py-16">
     <div class="container">
-        <div class="flex flex-wrap -mx-3">
-            <div class="w-full sm:w-1/2 px-3 mb-8 sm:mb-0">
+        <div class="flex flex-wrap justify-center text-center -mx-3">
+            <div class="px-3">
                 <a href="{{ $page->baseUrl }}">
                     <img src="/img/logo-dark.png" alt="{{ $page->company }} logo" width="150" class="mb-2">
                 </a>
-                <p class="text-sm text-grey">&copy; {{ date('Y') }} {{ $page->company }}. All rights reserved.</p>
-            </div>
-            <div class="flex-wrap flex w-full sm:w-1/2 px-3">
-                <div class="w-1/3">
-                    <h4 class="font-light pt-0 pb-4">Docs</h4>
-                    <ul class="list-reset">
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Docs link</a>
-                        </li>
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Docs link</a>
-                        </li>
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Docs link</a>
-                        </li>
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Docs link</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="w-1/3">
-                    <h4 class="font-light pt-0 pb-4">Company</h4>
-                    <ul class="list-reset">
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Company link</a>
-                        </li>
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Company link</a>
-                        </li>
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Company link</a>
-                        </li>
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Company link</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="w-1/3">
-                    <h4 class="font-light pt-0 pb-4">Resources</h4>
-                    <ul class="list-reset">
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Resource link</a>
-                        </li>
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Resource link</a>
-                        </li>
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Resource link</a>
-                        </li>
-                        <li>
-                            <a href="" class="text-sm text-grey hover:text-grey-dark leading-loose">Resource link</a>
-                        </li>
-                    </ul>
-                </div>
+                <p class="text-sm text-grey mb-0">&copy; {{ date('Y') }} {{ $page->company }}. All rights reserved.</p>
             </div>
         </div>
     </div>
